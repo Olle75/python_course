826d76397664f071cc9ea530789140741a8a5c94
Olle75 <olle.carlsson@landlantbruk.se>
2015-09-25 21:33:27 +0200

    Hemläxan, tillfixad
    Hej, här kommer en ny tillfixad version av hemläxan.
    Ska försöka ta itu med bonusuppgiften i helgen.
    
    Trevlig helg på er!
    Olle

--- day1/homework/robot_writer.py
+++ day1/homework/robot_writer.py
@@ -1,42 +1,60 @@
-# -*- coding: utf-8 -*-
-
-""" Se robot_writer.md för instruktioner
-"""
-
 # coding: utf-8
 
-kommun = "Hägersten"
+#Olles hemläxa!
+
 total_unemployment_2009 = 2.9
-total_unemployment_2014 = 5.9
-diff = total_unemployment_2014 - total_unemployment_2009
+total_unemployment_2014 = 1.9
+riket_unemployment_2009 = 3.8
+riket_unemployment_2014 = 10.9
+diff_riket2014 = riket_unemployment_2014 - total_unemployment_2014
 name = "Olle Carlsson"
+municipality = "Hägersten"
 
-def write_story(kommun, unemployment_2009, unemployment_2014):
-   text = "Arbetslösheten i %s var 2009 %s procent. 2014 var den %s procent." % (kommun, unemployment_2009, unemployment_2014)
-   print(text)
+def write_story(municipality, unemployment_2009, unemployment_2014):
+  diff = unemployment_2014 - unemployment_2009
+  text = "Arbetslösheten i %s var 2009 %s procent. 2014 var den %s procent." % (municipality, unemployment_2009, unemployment_2014)
+  print(text)
 
-write_story(kommun, total_unemployment_2009, total_unemployment_2014)
+  if unemployment_2014 > unemployment_2009:
+    print("Det var en ökning med %s procent.") % (abs(diff))
+  elif unemployment_2014 < unemployment_2009:
+    print("Det var en minskning med %s procent.") % (abs(diff))
+  elif unemployment_2014 == unemployment_2009:
+    print("Det var ingen skillnad")
 
-if total_unemployment_2014 > total_unemployment_2009:
-    print("Det var en ökning med %s procent" % (diff))   
-elif total_unemployment_2014 < total_unemployment_2009:
-	print("Det var en minskning med %s procent" % (diff))
-elif total_unemployment_2014 == total_unemployment_2009:
-   print("Det var ingen skillnad")
 
-print("%s, %s") % (name, kommun) 
+  if unemployment_2014 > unemployment_2009:
+    print("Alltså kan man konstatera att finanskrisen drabbade %s hårt." % (municipality))
+  elif unemployment_2014 < unemployment_2009:
+      print("Alltså kan man konstatera att %s klarade finanskrisen alldeles utmärkt." % (municipality))
+  elif unemployment_2014 == unemployment_2009:
+      print("Finanskrisen påverkade alltså inte %s alls." % (municipality))     
 
-    # Skriv kod här!
+
+  if unemployment_2014 > riket_unemployment_2014:
+    print("Arbetslösheten i %s var %s procent högre jämfört med hela riket 2014.") % (municipality, abs(diff_riket2014)) 
+  elif unemployment_2014 < riket_unemployment_2014:
+    print("Arbetslösheten i %s var %s procent lägre jämfört med hela riket 2014.") % (municipality, abs(diff_riket2014))
+  elif unemployment_2014 == riket_unemployment_2014:
+      print("Arbetslösheten i %s låg på samma nivå som i hela riket." % (municipality))
+
+  if unemployment_2014 < riket_unemployment_2014:
+    print("%s står starkt i krisen.") % (municipality)
+  elif unemployment_2014 > riket_unemployment_2014:
+    print("%s är en förlorare i krisen.") % (municipality)
+  elif unemployment_2014 == riket_unemployment_2014:
+    print("%s klarar krisen som alla andra.") % (municipality)
 
 
 
 write_story("Stockholm", 7.1, 6.6) 
+print("%s / %s") % (name, municipality)
 print("**************")
 
-write_story("Kiruna", 7.6, 4.2) 
+write_story("Kiruna", 7.6, 7.6) 
+print("%s / %s") % (name, municipality)
 print("**************")
 
 write_story("Lessebo", 9.5, 13.2) 
+print("%s / %s") % (name, municipality)
 print("**************")
-
-"""Källa: http://www.ekonomifakta.se/sv/Fakta/Regional-statistik/Din-kommun-i-siffror/"""
